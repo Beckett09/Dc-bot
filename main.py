9c16dada4dc184c0c899e7538c8f313cdbf837b5
Beckett09 <121704354+Beckett09@users.noreply.github.com>
2025-08-06 16:04:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from keep_alive import keep_alive
 load_dotenv()
 
 
-print("TOKEN:", os.environ.get('YOUR_VARIABLE_NAME'))
+print("TOKEN:", os.environ.get('TOKEN'))
 print("OWNER_ID:", os.getenv("OWNER_ID"))
 print("GUILD_ID:", os.getenv("GUILD_ID"))
 
